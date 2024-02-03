e7058299e59562b78f9cef0f419f7c768c1d72ab
Yash M <yashmahadik910@gmail.com>
2024-02-03 14:45:32 +0530

    halway commits

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
+const clearBtn = document.getElementById('clear');
+const itemFilter = document.getElementById('filter');
+
 
 function addItem(e) {
     e.preventDefault();
@@ -20,7 +23,10 @@ function addItem(e) {
     const button = createButton('remove-item btn-link text-red');
     li.appendChild(button);
 
+    // add li to the dom 
     itemList.appendChild(li);
+
+    checkUI();
 
     itemInput.value = '';
 }
@@ -41,5 +47,49 @@ function createIcon(classes) {
 }
 
 
+function removeItem(e) {
+    if (e.target.parentElement.classList.contains('remove-item')) {
+        if (confirm('Are you sure?')) {
+            e.target.parentElement.parentElement.remove();
+
+            checkUI();
+        }
+    }
+}
+
+
+function clearItems() {
+    while (itemList.firstChild) {
+        itemList.removeChild(itemList.firstChild);
+    }
+
+    checkUI();
+}
+
+
+function FilterItems(e) {
+    const text = e.target.value;
+
+    console.log(text);
+}
+
+
+function checkUI() {
+    const items = itemList.querySelectorAll('li');
+
+    if (items.length === 0) {
+        clearBtn.style.display = 'none';
+        itemFilter.style.display = 'none';
+    } else {
+        clearBtn.style.display = 'block';
+        itemFilter.style.display = 'block';
+    }
+}
+
 // Event Listeners
 itemForm.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
+clearBtn.addEventListener('click', clearItems);
+itemFilter.addEventListener('input', FilterItems);
+
+checkUI();
